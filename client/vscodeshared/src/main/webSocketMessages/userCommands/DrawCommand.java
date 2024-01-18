1f9028108dfdf14fca3a5a44f8910c72544a0cba
mewilker <129695902+mewilker@users.noreply.github.com>
2024-01-17 19:48:13 -0700

    Delete client/vscodeshared directory
    was a workaround in vscode for module dependency

--- client/vscodeshared/src/main/webSocketMessages/userCommands/DrawCommand.java
+++
@@ -1,9 +0,0 @@
-package webSocketMessages.userCommands;
-
-import model.AuthToken;
-
-public class DrawCommand extends UserGameCommand{
-    public DrawCommand(int id, AuthToken token) {
-        super(CommandType.DRAW, id, token);
-    }
-}
