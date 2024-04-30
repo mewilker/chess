8e79c25a36e4241cc61320a54aa0c54fa4ec3d05
mewilker <mewilk@student.byu.edu>
2024-04-30 15:49:16 -0600

    port Move to ChessMove

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -8,22 +8,29 @@ package chess;
  */
 public class ChessMove {
 
+    ChessPosition startPos;
+    ChessPosition endPos;
+    ChessPiece.PieceType promotion;
+
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
                      ChessPiece.PieceType promotionPiece) {
+        startPos = startPosition;
+        endPos = endPosition;
+        promotion = promotionPiece;
     }
 
     /**
      * @return ChessPosition of starting location
      */
     public ChessPosition getStartPosition() {
-        throw new RuntimeException("Not implemented");
+        return startPos;
     }
 
     /**
      * @return ChessPosition of ending location
      */
     public ChessPosition getEndPosition() {
-        throw new RuntimeException("Not implemented");
+        return endPos;
     }
 
     /**
@@ -33,6 +40,61 @@ public class ChessMove {
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
     public ChessPiece.PieceType getPromotionPiece() {
-        throw new RuntimeException("Not implemented");
+        return promotion;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = startPos.getRow()*31+ endPos.getRow();
+        result *= startPos.getColumn() + endPos.getColumn();
+        return result;
+    }
+
+    @Override
+    /**
+     * @return true if startpos, endpos and promotion are the same */
+    public boolean equals(Object obj) {
+        if (this == obj){
+            return true;
+        }
+        if (obj == null){
+            return false;
+        }
+        if (getClass() != obj.getClass()){
+            return false;
+        }
+        ChessMove other = (ChessMove) obj;
+        if (startPos == null) {
+            if (other.startPos != null)
+                return false;
+        }
+        else if (!startPos.equals(other.startPos)){
+            return false;
+        }
+        if (endPos == null) {
+            if (other.endPos != null)
+                return false;
+        }
+        else if (!endPos.equals(other.endPos)){
+            return false;
+        }
+        if (promotion != other.promotion)
+            return false;
+        return true;
+    }
+
+    @Override
+    public ChessMove clone(){
+        return new ChessMove(this.startPos, this.endPos, this.promotion);
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder out = new StringBuilder("StartPos:");
+        out.append(startPos.toString()+'\n');
+        out.append("EndPos:");
+        out.append(endPos.toString()+'\n');
+        return out.toString();
     }
 }
