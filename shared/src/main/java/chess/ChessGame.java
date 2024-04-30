227f1e119c517bd98507a10506f5211784924055
mewilker <mewilk@student.byu.edu>
2024-04-30 17:44:20 -0600

    Passes Current Phase 1 specs, initializes game in ChessGame, broke apart isinStaleMate

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.HashSet;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -9,16 +10,19 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessGame {
-
+    TeamColor teamTurn = TeamColor.WHITE;
+    ChessBoard playBoard;
+    TeamColor winner = null;
     public ChessGame() {
-
+        playBoard = new ChessBoard();
+        playBoard.resetBoard();
     }
 
     /**
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+        return teamTurn;
     }
 
     /**
@@ -27,7 +31,7 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-        throw new RuntimeException("Not implemented");
+        teamTurn = team;
     }
 
     /**
@@ -46,7 +50,27 @@ public class ChessGame {
      * startPosition
      */
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
-        throw new RuntimeException("Not implemented");
+        ChessPiece piece = (ChessPiece) playBoard.getPiece(startPosition);
+        Collection<ChessMove> moves = piece.pieceMoves(playBoard, startPosition.clone());
+        HashSet<ChessMove> possmoves = new HashSet<>(moves);//makes a flippin deep clone of the piece moves
+        HashSet<ChessMove> validmoves = new HashSet<>();
+        for (ChessMove move : possmoves){
+            ChessBoard board = playBoard.clone();
+            try{
+                playBoard.movePiece(move.clone());
+                if(!isInCheck(piece.getTeamColor())){
+                    validmoves.add(move.clone());
+                }
+            }
+            catch(InvalidMoveException e){
+                System.out.println("program attempted to move a piece not there");
+                //validmoves.remove(move);
+            }
+            finally{
+                setBoard(board);
+            }
+        }
+        return validmoves;
     }
 
     /**
@@ -56,7 +80,22 @@ public class ChessGame {
      * @throws InvalidMoveException if move is invalid
      */
     public void makeMove(ChessMove move) throws InvalidMoveException {
-        throw new RuntimeException("Not implemented");
+        boolean invalidMove = true;
+        ChessPosition start = move.getStartPosition().clone();
+        if(playBoard.getPiece(start) != null && playBoard.getPiece(start).getTeamColor()==teamTurn){
+            Collection<ChessMove> moves = validMoves(start.clone());
+            for (ChessMove moveitr : moves){
+                if (moveitr.equals(move)){
+                    invalidMove = false;
+                    playBoard.movePiece(move);
+                    advanceTurn();
+                    return;
+                }
+            }
+        }
+        if (invalidMove){
+            throw new InvalidMoveException();
+        }
     }
 
     /**
@@ -66,7 +105,19 @@ public class ChessGame {
      * @return True if the specified team is in check
      */
     public boolean isInCheck(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        TeamColor opponent = opponentColor(teamColor);
+        Collection<ChessMove> moves = new HashSet<>();
+        Collection<ChessPosition> places = playBoard.piecesOnBoard(opponent);
+        for (ChessPosition place : places){
+            ChessPiece piece = playBoard.getPiece(place);
+            moves= piece.pieceMoves(playBoard, place.clone());
+            for (ChessMove move : moves){
+                if(move.getEndPosition().equals(playBoard.kingPos(teamColor))){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     /**
@@ -76,7 +127,14 @@ public class ChessGame {
      * @return True if the specified team is in checkmate
      */
     public boolean isInCheckmate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        if (!isInCheck(teamColor)){
+            return false;
+        }
+        if (hasNoMovesToMake(teamColor)){
+            setwinner(teamColor);
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -87,7 +145,20 @@ public class ChessGame {
      * @return True if the specified team is in stalemate, otherwise false
      */
     public boolean isInStalemate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+      return !isInCheckmate(teamColor) && hasNoMovesToMake(teamColor);
+    }
+
+    /** helper method determining if there are moves left on the board*/
+    public boolean hasNoMovesToMake(TeamColor teamColor) {
+        ChessBoard board = playBoard.clone();
+        Collection<ChessPosition> places = board.piecesOnBoard(teamColor);
+        for (ChessPosition place : places){
+            Collection<ChessMove> moves = validMoves(place.clone());
+            if(moves.size()>0){
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
@@ -96,7 +167,7 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        throw new RuntimeException("Not implemented");
+        playBoard = board;
     }
 
     /**
@@ -105,6 +176,39 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return playBoard;
+    }
+
+    /**
+     *
+     * @param teamColor
+     * @return opposing teams color
+     */
+    private TeamColor opponentColor(TeamColor teamColor){
+        TeamColor opponent;
+        if (teamColor == TeamColor.WHITE){
+            opponent = TeamColor.BLACK;
+        }
+        else{
+            opponent = TeamColor.WHITE;
+        }
+        return opponent;
+    }
+
+    /**changes the turn to the other team */
+    private void advanceTurn(){
+        if (teamTurn == TeamColor.WHITE){
+            teamTurn = TeamColor.BLACK;
+            return;
+        }
+        teamTurn = TeamColor.WHITE;
+    }
+
+    public TeamColor winner(){
+        return winner;
+    }
+
+    public void setwinner(TeamColor color){
+        winner = opponentColor(color);
     }
 }
